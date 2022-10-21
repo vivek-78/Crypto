202191f080b3173984457a7e5af04ca8447799e8
rahul <rsrsrahul959@gmail.com>
2022-10-21 23:26:08 +0530

    added the rows

--- src/crypto/list/Crypto.js
+++ src/crypto/list/Crypto.js
@@ -10,24 +10,8 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 const Crypto = () => {
   const [data,setData]= useState({});
-  axios.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH&tsyms=USD&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146")
-  .then((res)=>{
-     setData(res.data.DISPLAY.BTC.USD);
-  });
-function createData(
-  logo,
-  name,
-  price,
-  gainPercent,
-  highestIn24h,
-  lowestIn24h
-  
-) {
-  return { logo, name, price, gainPercent, highestIn24h, lowestIn24h};
-}
-  const rows=[
-    createData(data.IMAGEURL,"BitCoin",data.PRICE,data.CHANGEPCT24HOUR,data.HIGH24HOUR,data.LOW24HOUR)
-  ]
+  const coins=["BTC"];
+  const url = "https://www.cryptocompare.com/";
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -42,21 +26,28 @@ function createData(
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
+        {coins.map(element => {
+  axios.get(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${element}&tsyms=USD&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146`)
+  .then((res)=>{
+    const key=res.data.DISPLAY;
+     setData(key[element].USD);
+  });
+    return(
+    <TableRow
+              key={element}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                <img src="https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg" height="40px" alt=""></img>
+                <img src={url+data.IMAGEURL} height="40px" alt=""></img>
               </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.price}</TableCell>
-              <TableCell align="right">{row.gainPercent}% {row.gainPercent ? <BsArrowUp /> : <BsArrowDown/>}</TableCell>
-              <TableCell align="right">{row.highestIn24h}</TableCell>
-              <TableCell align="right">{row.lowestIn24h}</TableCell>
+              <TableCell align="right">{element}</TableCell>
+              <TableCell align="right">{data.PRICE}</TableCell>
+              <TableCell align="right">{data.CHANGEPCT24HOUR}% {data.CHANGEPCT24HOUR>0 ? <BsArrowUp /> : <BsArrowDown/>}</TableCell>
+              <TableCell align="right">{data.HIGH24HOUR}</TableCell>
+              <TableCell align="right">{data.LOW24HOUR}</TableCell>
             </TableRow>
-          ))}
+            );
+  })}
         </TableBody>
       </Table>
     </TableContainer>
