c0b7a8c3b554b5b4219c3a25010e8d54d2e06c25
vivek-78 <akepoguvivek14@gmail.com>
2022-10-23 20:56:08 +0530

    created coins list

--- src/crypto/list/Crypto.js
+++ src/crypto/list/Crypto.js
@@ -8,55 +8,25 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import CryptoList from './CryptoList';
+
 const Crypto = () => {
-  const [data,setData]= useState({});
-  axios.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH&tsyms=USD&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146")
-  .then((res)=>{
-     setData(res.data.DISPLAY.BTC.USD);
-  });
-function createData(
-  logo,
-  name,
-  price,
-  gainPercent,
-  highestIn24h,
-  lowestIn24h
-  
-) {
-  return { logo, name, price, gainPercent, highestIn24h, lowestIn24h};
-}
-  const rows=[
-    createData(data.IMAGEURL,"BitCoin",data.PRICE,data.CHANGEPCT24HOUR,data.HIGH24HOUR,data.LOW24HOUR)
-  ]
+  const coins = ["BTC","ETH","BNB","BCH"];
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Logo</TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Price</TableCell>
-            <TableCell align="right">Change Percentage</TableCell>
-            <TableCell align="right">Highest Today</TableCell>
-            <TableCell align="right">Lowest Today</TableCell>
+            <TableCell align="left">Name</TableCell>
+            <TableCell align="left">Price</TableCell>
+            <TableCell align="left">Change Percentage</TableCell>
+            <TableCell align="left">Highest Today</TableCell>
+            <TableCell align="left">Lowest Today</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                <img src="https://s3.us-east-2.amazonaws.com/nomics-api/static/images/currencies/btc.svg" height="40px" alt=""></img>
-              </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.price}</TableCell>
-              <TableCell align="right">{row.gainPercent}% {row.gainPercent ? <BsArrowUp /> : <BsArrowDown/>}</TableCell>
-              <TableCell align="right">{row.highestIn24h}</TableCell>
-              <TableCell align="right">{row.lowestIn24h}</TableCell>
-            </TableRow>
-          ))}
+            {coins.map((coin)=><CryptoList coin={coin} />)}
         </TableBody>
       </Table>
     </TableContainer>
---
+++ src/crypto/list/CryptoList.jsx
@@ -0,0 +1,35 @@
+import {React,useState,useEffect} from "react";
+import axios from "axios";
+import {TableRow,TableCell} from "@mui/material";
+import {BsArrowUp,BsArrowDown} from "react-icons/bs";
+
+const CryptoList = (props)=>{
+    const {coin} = props
+    const [coinData,setCoinData] = useState({});
+    useEffect(()=>{
+        async function fetchData(){
+            const fetchedData = await axios.get(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${coin}&tsyms=USD&api_key=0646cc7b8a4d4b54926c74e0b20253b57fd4ee406df79b3d57d5439874960146`)
+            const data = fetchedData.data.DISPLAY[coin];
+            setCoinData(data.USD);
+        }
+        fetchData();
+    },
+    []);
+    return(
+        <TableRow
+        key={coinData.name}
+        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">
+          <img src={`https://www.cryptocompare.com${coinData.IMAGEURL}`} width={50} height={50} alt=""></img>
+        </TableCell>
+        <TableCell align="left">{"BitCoin"}</TableCell>
+        <TableCell align="left">{coinData.PRICE}</TableCell>
+        <TableCell align="left">{coinData.CHANGEPCT24HOUR}% {coinData.CHANGEPCT24HOUR > 0 ? <BsArrowUp /> : <BsArrowDown/>}</TableCell>
+        <TableCell align="left">{coinData.HIGH24HOUR}</TableCell>
+        <TableCell align="left">{coinData.LOW24HOUR}</TableCell>
+      </TableRow>
+    )
+}
+
+export default CryptoList;
