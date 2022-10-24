870ccf36acf0dd7fa9fe842a082f0e2449150815
vivek-78 <akepoguvivek14@gmail.com>
2022-10-24 10:30:10 +0530

    added key to list

--- src/crypto/list/Crypto.js
+++ src/crypto/list/Crypto.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,Paper
+} from '@mui/material';
 import CryptoList from './CryptoList';
 
 const Crypto = () => {
@@ -25,7 +26,7 @@ const Crypto = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-            {coins.map((coin)=><CryptoList coin={coin} />)}
+            {coins.map((coin)=><CryptoList coin={coin} key={coin}/>)}
         </TableBody>
       </Table>
     </TableContainer>
