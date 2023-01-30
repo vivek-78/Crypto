49e5012f15b380c612c27a379e36e7b10d231d59
rahul <rsrsrahul959@gmail.com>
2023-01-30 21:03:11 +0530

    graphs

--- src/crypto/detail/cryptoDetail.js
+++ src/crypto/detail/cryptoDetail.js
@@ -1,75 +1,54 @@
 import { React, useEffect, useState } from 'react';
-import Chart from 'react-apexcharts';
 import axios from 'axios';
 import { Grid } from '@mui/material';
+import ApexCharts from 'apexcharts';
 import { useParams } from 'react-router-dom';
 
 const CryptoDetail = () => {
   const { coin } = useParams();
-  const [chartData, setChartData] = useState({});
+  const [data, setData] = useState([]);
   useEffect(() => {
-    function fetchData() {
-      axios
-        .get(
-          `https://min-api.cryptocompare.com/data/v2/histominute?fsym=${coin}&tsym=USD&limit=80&api_key=557770814a82703ce2ed50c174c03264fee9a0117e1dc109f892d1a4f82084fc`
-        )
-        .then((fetchedData) => {
-          const data = fetchedData.data.Data.Data;
-          const times = data.map((obj) => new Date(obj.time * 1000).getHours());
-          const prices = data.map((obj) => obj.close);
-          setChartData({ times, prices });
-        });
-    }
+    const fetchData = async () => {
+      const result = await axios.get(
+        `https://min-api.cryptocompare.com/data/v2/histoday?fsym=${coin}&tsym=USD&limit=1000`
+      );
+      setData(result.data.Data.Data);
+    };
     fetchData();
-  });
-  const { prices, times } = chartData;
+  }, []);
+  useEffect(() => {
+    const options = {
+      chart: {
+        type: 'area',
+        height: 500,
+        width: 1500
+      },
+      xaxis: {
+        type: 'datetime'
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'smooth',
+        width: 2
+        // colors: '#51db5a'
+      },
+      series: [
+        {
+          name: coin,
+          data: data.map((item) => [item.time * 1000, item.close])
+        }
+      ]
+    };
+
+    const chart = new ApexCharts(document.querySelector('#chart'), options);
+    chart.render();
+  }, [data]);
+
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Chart
-          type="line"
-          width={1349}
-          height={550}
-          series={[
-            {
-              name: coin + '(USD)',
-              data: prices
-            }
-          ]}
-          options={{
-            title: {
-              text: coin,
-              align: 'center',
-              margin: 10,
-              offsetX: 0,
-              offsetY: 0,
-              floating: false,
-              style: {
-                fontSize: '24px',
-                color: '#213043'
-              }
-            },
-            stroke: {
-              show: true,
-              curve: 'smooth',
-              lineCap: 'butt',
-              colors: '#51db5a',
-              width: 2
-            },
-            chart: {
-              id: 'bar-chart',
-              toolbar: {
-                show: false
-              }
-            },
-            grid: {
-              show: false
-            },
-            xaxis: {
-              categories: times
-            }
-          }}></Chart>
-      </Grid>
+    <Grid>
+      <div id="chart" />
     </Grid>
   );
 };
