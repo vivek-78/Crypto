1b2d4b621e7c29f57deb8627ab37ea82a7aaa2d9
Rahul <rsrsrahul959@gmail.com>
2023-07-02 13:04:00 +0530

    task: almost done with the news component

--- src/crypto/list/Crypto.js
+++ src/crypto/list/Crypto.js
@@ -1,5 +1,5 @@
-import { React, useEffect, useState } from 'react';
-import axios from 'axios';
+import { React, useEffect, useState } from "react";
+import axios from "axios";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -11,13 +11,21 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Grid,
   Typography
-} from '@mui/material';
-import Development from '../../development';
-import CryptoList from './CryptoList';
-import { CryptoNews } from './components';
+} from "@mui/material";
+import Development from "../../development";
+import CryptoList from "./CryptoList";
+import { CryptoNews } from "./components";
 
 const Crypto = () => {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  };
   const [newsData, setNewsData] = useState({});
   var count = 0;
   useEffect(() => {
@@ -34,42 +42,44 @@ const Crypto = () => {
     return <Development />;
   }
   const coins = [
-    'BTC',
-    'ETH',
-    'SHIB',
-    'BNB',
-    'ADA',
-    'XLM',
-    'TRX',
-    'USDT',
-    'THETA',
-    'SOL',
-    'MATIC',
-    'ETC',
-    'DASH',
-    'ZEC'
+    "BTC",
+    "ETH",
+    "SHIB",
+    "BNB",
+    "ADA",
+    "XLM",
+    "TRX",
+    "USDT",
+    "THETA",
+    "SOL",
+    "MATIC",
+    "ETC",
+    "DASH",
+    "ZEC"
   ];
   return (
     <Grid>
-      <Grid item xs={12}>
+      <Grid item xs={12} sx={{ textAlign: "center" }}>
         <Typography variant="h5">Latest News</Typography> <br />
       </Grid>
-      <Grid container direction="row" spacing={2} sx={{ marginLeft: 2 }} className="con">
-        {Array.isArray(newsData)
-          ? newsData.map((data) => (
-              <Grid item key={data.id}>
-                <CryptoNews
-                  count={++count}
-                  src={data.source_info.name}
-                  title={data.title}
-                  url={data.url}
-                  image={data.imageurl}
-                />
-              </Grid>
-            ))
-          : ''}
+      <Grid sx={{ marginLeft: 0, marginBottom: 2 }}>
+        <Slider {...settings}>
+          {Array.isArray(newsData)
+            ? newsData.map((data) => (
+                <div key={data.id}>
+                  <CryptoNews
+                    count={++count}
+                    src={data.source_info.name}
+                    title={data.title}
+                    url={data.url}
+                    image={data.imageurl}
+                  />
+                </div>
+              ))
+            : ""}
+        </Slider>
       </Grid>
-      <Grid item>
+      <Grid item sx={{ marginTop: 5 }}>
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
@@ -78,9 +88,9 @@ const Crypto = () => {
                 <TableCell align="center">Name</TableCell>
                 <TableCell align="center">Price</TableCell>
                 <TableCell align="center">Change Percentage</TableCell>
-                <TableCell align="left">Highest Today</TableCell>
-                <TableCell align="left">Lowest Today</TableCell>
-                <TableCell align="left">Market Cap</TableCell>
+                <TableCell align="center">Highest Today</TableCell>
+                <TableCell align="center">Lowest Today</TableCell>
+                <TableCell align="center">Market Cap</TableCell>
                 <TableCell align="left">Last Hour</TableCell>
               </TableRow>
             </TableHead>
